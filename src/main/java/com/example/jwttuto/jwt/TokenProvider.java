7c64b89ed0955646cc2e4ff06ab902ca2050f39b
gur8624 <77798971+gur8624@users.noreply.github.com>
2021-04-03 18:10:14 +0900

    수정조끔함

--- src/main/java/com/example/jwttuto/jwt/TokenProvider.java
+++ src/main/java/com/example/jwttuto/jwt/TokenProvider.java
@@ -3,6 +3,7 @@ package com.example.jwttuto.jwt;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.log4j.Log4j2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
@@ -20,28 +21,28 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.stream.Collectors;
 
+@Log4j2
 @Component
 public class TokenProvider implements InitializingBean { // 빈이  생성되고 주입받은 후에 secret값을 base64에 decode해서 key변수에 할당
 
-    private final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
 
     private static final String AUTHORITIES_KEY = "auth";
 
     private final String secret;
     private final long tokenValidityInMilliseconds;
 
-    private Key key;
+    private Key key; // 내부Key
 
 
-    public TokenProvider(
+    public TokenProvider( // 프로퍼티 메모값 가져옴
             @Value("${jwt.secret}") String secret,
             @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSeconds) {
         this.secret = secret;
-        this.tokenValidityInMilliseconds = tokenValidityInSeconds * 1000;
+        this.tokenValidityInMilliseconds = tokenValidityInSeconds * 1000; // 밀리초 계산
     }
 
     @Override
-    public void afterPropertiesSet() {
+    public void afterPropertiesSet() { // 암호화
         byte[] keyBytes = Decoders.BASE64.decode(secret);
         this.key = Keys.hmacShaKeyFor(keyBytes);
     }
@@ -50,15 +51,16 @@ public class TokenProvider implements InitializingBean { // 빈이  생성되고
         String authorities = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(","));
+        log.info("값1234455"+ authorities); // 로그인한 유저 권한 얻어오기
 
         long now = (new Date()).getTime();
-        Date validity = new Date(now + this.tokenValidityInMilliseconds);
+        Date validity = new Date(now + this.tokenValidityInMilliseconds); // 유효기간 설정 (지금은 24시간으로설정됨)
 
         return Jwts.builder()
-                .setSubject(authentication.getName())
-                .claim(AUTHORITIES_KEY, authorities)
-                .signWith(key, SignatureAlgorithm.HS512)
-                .setExpiration(validity)
+                .setSubject(authentication.getName()) // 제목
+                .claim(AUTHORITIES_KEY, authorities) // 권한
+                .signWith(key, SignatureAlgorithm.HS512) // 알고리즘 유형
+                .setExpiration(validity) // 유효기간
                 .compact();
     }
 
@@ -85,13 +87,13 @@ public class TokenProvider implements InitializingBean { // 빈이  생성되고
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
             return true;
         } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
-            logger.info("잘못된 JWT 서명입니다.");
+            log.info("잘못된 JWT 서명입니다.");
         } catch (ExpiredJwtException e) {
-            logger.info("만료된 JWT 토큰입니다.");
+            log.info("만료된 JWT 토큰입니다.");
         } catch (UnsupportedJwtException e) {
-            logger.info("지원되지 않는 JWT 토큰입니다.");
+            log.info("지원되지 않는 JWT 토큰입니다.");
         } catch (IllegalArgumentException e) {
-            logger.info("JWT 토큰이 잘못되었습니다.");
+            log.info("JWT 토큰이 잘못되었습니다.");
         }
         return false;
     }
